24fb1d0124ee3e58bd2eb8128b8259b44e3eb7c0
ivozzo <alessandro.ivaldi@email.it>
2021-02-17 13:23:05 +0100

    Fix typo

--- economy-bot/modules/configuration.py
+++ economy-bot/modules/configuration.py
@@ -9,6 +9,6 @@ logging_level = os.getenv("ECONOMY_LOGGING_LEVEL")
 
 database_user = os.getenv("ECONOMY_DATABASE_USER")
 database_password = os.getenv("ECONOMY_DATABASE_PASSWORD")
-database__name = os.getenv("ECONOMY_DATABASE")
+database_name = os.getenv("ECONOMY_DATABASE")
 database_host = os.getenv("ECONOMY_DATABASE_HOST")
 database_configuration_collection = os.getenv("ECONOMY_DATABASE_CONFIGURATION_COLLECTION")
